fixed latent statistics computation in VAE

build.py
@@ -21,6 +21,9 @@ def log1px_encode(x):
     return torch.sign(x) * torch.log(1 + torch.abs(x))
 
 def dataset(name):
+    def normalize(x):
+        return (x - x.mean()) / x.std()
+    
     if name == 'mnist':
         return datasets.MNIST(root='mnist', train=True, transform=transforms.Compose([transforms.ToTensor(), transforms.Resize(32)]), download=True)
     elif name == 'dataset_v2':
@@ -39,7 +42,7 @@ def dataset(name):
         ensure_download('X_v4', 'X_v4.p')
         ensure_download('y_v4', 'y_v4.p')
         X, y = torch.load('X_v4.p'), torch.load('y_v4.p')
-        return [(log1px_encode(X[i]).unsqueeze(0), y[i]) for i in range(len(X))]
+        return [(normalize(X[i]).unsqueeze(0), y[i]) for i in range(len(X))]
     elif name == 'dataset_v4_overfit':
         return dataset('dataset_v4')[:4]
     else:
@@ -71,7 +74,8 @@ def criterion(*, k_mse=None, k_kl=None, **ignore):
     else:
         def result(input, target, aux, info):
             mse = F.mse_loss(input, target)
-            kl = kl_div(input, aux)
+            mu, logsigma = aux
+            kl = kl_div(mu, logsigma)
             return k_mse * mse + k_kl * kl, {'mse': mse, 'kl': kl}
         return result
 
@@ -134,12 +138,12 @@ def saved_model(run_name, checkpoint):
         run=run_name,
         mode='read-only'
     )
-    model = checkpoint + '.p'
-    if model not in os.listdir():
-        run[checkpoint].download(model)
+    filename = checkpoint + '.p'
+    if filename not in os.listdir():
+        run[checkpoint].download(filename)
     params = run['params'].fetch()
     params['device'] = 'cpu'
     print(params)
     model, _, _ = model_optim_sched(**params)
-    model.load_state_dict(torch.load('model.p', map_location='cpu'))
+    model.load_state_dict(torch.load(filename, map_location='cpu'))
     return model

models.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import isinteractive
 from sklearn.neighbors import KNeighborsTransformer
 import torch
 from torch import nn, optim
@@ -62,7 +63,7 @@ def ResLearned(conv_type, in_channels, out_channels, in_size, out_size, kernel_s
     return Sum(
         Sequential(
             BatchNorm2d(in_channels),
-            conv_type(in_channels, out_channels, in_size, out_size, kernel_size, stride ** 2, bias=False)
+            conv_type(in_channels, out_channels, in_size, out_size, stride ** 2, stride ** 2, bias=False)
         ),
         Sequential(
             ConvBlock(conv_type, in_channels, mid_channels, in_size, mid_size, kernel_size, stride),
@@ -77,18 +78,28 @@ class VAE(nn.Module):
         self.mu_head = mu_head
         self.ls_head = ls_head
         self.decoder = decoder
-    
-        self.last_z = None
-        self.z_mean = 0
-        self.z_std = 0
-        self.z_cnt = 0
+        self.register_buffer('last_z', None)
+        self.register_buffer('z_mean', None)
+        self.register_buffer('z_m2', None)
+        self.register_buffer('z_std', None)
+        self.register_buffer('z_cnt', None)
     
     def note_z(self, z):
         z = z[0:1]
         self.last_z = z
-        self.z_mean = (self.z_mean * self.z_cnt + z) / (self.z_cnt + 1)
-        self.z_std = (self.z_std * self.z_cnt + z) / (self.z_cnt + 1)
-        self.z_cnt += 1
+        if self.z_cnt is None:
+            self.z_mean = z
+            self.z_m2 = torch.zeros_like(z)
+            self.z_std = torch.zeros_like(z)
+            self.z_cnt = torch.tensor(1.0)
+        else:
+            # update from https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+            self.z_cnt += 1
+            delta = z - self.z_mean
+            self.z_mean += delta / self.z_cnt
+            delta2 = z - self.z_mean
+            self.z_m2 += delta * delta2
+            self.z_std = torch.sqrt(self.z_m2 / self.z_cnt)
 
     def encode(self, x):
         t = self.encoder(x)